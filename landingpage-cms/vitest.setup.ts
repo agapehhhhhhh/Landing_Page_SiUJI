0b8a0c728560c50909e6f0edf2804a02500b6016
agapehhhhhhh <114454472+agapehhhhhhh@users.noreply.github.com>
2025-07-25 11:16:56 +0700

    Delete landingpage-cms directory

--- landingpage-cms/vitest.setup.ts
+++
@@ -1,8 +0,0 @@
-// File: landingpage-cms/vitest.setup.ts
-import { config } from 'dotenv'
-import path from 'path'
-
-// Load .env dari parent directory
-config({ path: path.resolve(__dirname, '../.env') })
-
-// Any other setup scripts you might need go here
